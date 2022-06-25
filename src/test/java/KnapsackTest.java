45518d258801bb42efd547875284ed12259fcf46
2000game <felix.kotschenreuther@gmail.com>
2022-06-25 17:44:02 +0200

    Added Runtime Tests for the Worst Case Complexity of knapsack.backtracking

--- src/test/java/KnapsackTest.java
+++ src/test/java/KnapsackTest.java
@@ -1,10 +1,8 @@
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class knapsackTest {
-
+class KnapsackTest {
     @Test
     void backtracking1() {
         int[] gewichte = {10, 5, 7, 11, 13, 1, 7, 11, 13, 19, 19, 9, 8, 2, 7, 31};
@@ -17,6 +15,7 @@ class knapsackTest {
         assertArrayEquals(new int[]{0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0}, ausgewaehlt);
 
     }
+
     @Test
     void backtracking2() {
         int[] gewichte = {40, 40, 40, 40};
@@ -35,9 +34,7 @@ class knapsackTest {
         int[] werte = {7, 6, 1, 1, 1, 4, 11, 20, 3, 7, 8, 9, 16, 19, 100, 3};
         int maxgewicht = 30;
         knapsack k = new knapsack(gewichte, werte, maxgewicht);
-        assertThrows(IllegalArgumentException.class, () -> {
-            k.backtracking();
-        });
+        assertThrows(IllegalArgumentException.class, k::backtracking);
     }
 
     @Test
@@ -46,9 +43,7 @@ class knapsackTest {
         int[] werte = {};
         int maxgewicht = 30;
         knapsack k = new knapsack(gewichte, werte, maxgewicht);
-        assertThrowsExactly(IllegalArgumentException.class, () -> {
-            k.backtracking();
-        });
+        assertThrowsExactly(IllegalArgumentException.class, k::backtracking);
     }
 
     @Test
@@ -57,9 +52,7 @@ class knapsackTest {
         int[] werte = {7, 6, 1, 1, 1, 4, 11, 20, 3, 7, 8, 9, 16, 19, 100, 3};
         int maxgewicht = -1;
         knapsack k = new knapsack(gewichte, werte, maxgewicht);
-        assertThrows(IllegalArgumentException.class, () -> {
-            k.backtracking();
-        });
+        assertThrows(IllegalArgumentException.class, k::backtracking);
     }
 
     @Test
@@ -86,6 +79,15 @@ class knapsackTest {
         assertArrayEquals(new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, ausgewaehlt);
     }
 
+    @Test
+    void backtracking8() {
+        int[] gewichte = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        int[] werte = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int maxgewicht = 30;
+        knapsack k = new knapsack(gewichte, werte, maxgewicht);
+        k.backtracking();
+    }
+
 
     @Test
     void greedy1() {
@@ -99,6 +101,7 @@ class knapsackTest {
         assertArrayEquals(new int[]{0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0}, ausgewaehlt);
 
     }
+
     @Test
     void greedy2() {
         int[] gewichte = {40, 40, 40, 40};
@@ -117,9 +120,7 @@ class knapsackTest {
         int[] werte = {7, 6, 1, 1, 1, 4, 11, 20, 3, 7, 8, 9, 16, 19, 100, 3};
         int maxgewicht = 30;
         knapsack k = new knapsack(gewichte, werte, maxgewicht);
-        assertThrows(IllegalArgumentException.class, () -> {
-            k.backtracking();
-        });
+        assertThrows(IllegalArgumentException.class, k::backtracking);
     }
 
     @Test
@@ -128,9 +129,7 @@ class knapsackTest {
         int[] werte = {};
         int maxgewicht = 30;
         knapsack k = new knapsack(gewichte, werte, maxgewicht);
-        assertThrowsExactly(IllegalArgumentException.class, () -> {
-            k.backtracking();
-        });
+        assertThrowsExactly(IllegalArgumentException.class, k::backtracking);
     }
 
     @Test
@@ -139,9 +138,7 @@ class knapsackTest {
         int[] werte = {7, 6, 1, 1, 1, 4, 11, 20, 3, 7, 8, 9, 16, 19, 100, 3};
         int maxgewicht = -1;
         knapsack k = new knapsack(gewichte, werte, maxgewicht);
-        assertThrows(IllegalArgumentException.class, () -> {
-            k.backtracking();
-        });
+        assertThrows(IllegalArgumentException.class, k::backtracking);
     }
 
     @Test
@@ -167,4 +164,65 @@ class knapsackTest {
         assertEquals(16, k.wert_beladung);
         assertArrayEquals(new int[]{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}, ausgewaehlt);
     }
+
+    @Test
+    void greedy8() {
+        int[] gewichte = {7, 6, 1, 1, 1, 4, 11, 20, 3, 7, 8, 9, 16, 19, 100, 3};
+        int[] werte = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int maxgewicht = 30;
+        knapsack k = new knapsack(gewichte, werte, maxgewicht);
+        k.backtracking();
+    }
+
+    void worst_case_knapsack_runtime(int array_length) {
+        double start;
+        double finish;
+        int[] gewichte = new int[array_length];
+        int[] werte = new int[array_length];
+        for (int i = 0; i < 10; i++) {
+            gewichte[i] = 1;
+            werte[i] = 1;
+        }
+        knapsack k = new knapsack(gewichte, werte, array_length);
+        start = System.nanoTime();
+        k.backtracking();
+        finish = System.nanoTime();
+        System.out.println();
+        System.out.println(array_length + "-Array Laenge backtracking\nDauer: " + (finish - start) / 1000000.0 + " ms\n");
+        System.out.println();
+    }
+
+    @Test
+    void worst_case_knapsack_runtime_10() {
+        worst_case_knapsack_runtime(10);
+    }
+
+    @Test
+    void worst_case_knapsack_runtime_20() {
+        worst_case_knapsack_runtime(20);
+    }
+
+    @Test
+    void worst_case_knapsack_runtime_50() {
+        worst_case_knapsack_runtime(50);
+    }
+
+    @Test
+    void worst_case_knapsack_runtime_100() {
+        worst_case_knapsack_runtime(100);
+    }
+    @Test
+    void worst_case_knapsack_runtime_200() {
+        worst_case_knapsack_runtime(200);
+    }
+    @Test
+    void worst_case_knapsack_runtime_500() {
+        worst_case_knapsack_runtime(500);
+    }
+    @Test
+    void worst_case_knapsack_runtime_1000() {
+        worst_case_knapsack_runtime(1000);
+    }
+
+
 }

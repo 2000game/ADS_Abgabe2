ca990ed4b99b36e1fa4c73570836e464088256c9
2000game <felix.kotschenreuther@gmail.com>
2022-06-25 17:53:54 +0200

    Added Comparison of Backtracking and Greedy in main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,33 +1,47 @@
+import java.util.Arrays;
+import java.util.OptionalDouble;
+import java.util.Random;
+
 public class main {
+   static Random r = new Random();
     public static void main(String[] args) {
-        // build random knapsack
+        // build knapsack problem
         int[] gewichte = {10, 5, 7, 11, 13, 1, 7, 11, 13, 19, 19, 9, 8, 2, 7, 31};
         int[] werte = {7, 6, 1, 1, 1, 4, 11, 20, 3, 7, 8, 9, 16, 19, 100, 3};
         int maxgewicht = 30;
-        knapsack k = new knapsack(gewichte, werte, maxgewicht);
-        int[] ausgewaehlt = k.greedyRucksack();
-        System.out.println("Gewicht: " + k.gewicht_beladung);
-        System.out.println("Wert: " + k.wert_beladung);
-        System.out.print("Ausgewaehlt: ");
-        for (int j : ausgewaehlt) {
-            System.out.print(j + " ");
-        }
-        System.out.println();
+        knapsack backtracking_knapsack = new knapsack(gewichte, werte, maxgewicht);
+        knapsack greedy_knapsack = new knapsack(gewichte, werte, maxgewicht);
+        int[] backtracking_solution = backtracking_knapsack.backtracking();
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Backtracking Rucksack: " + Arrays.toString(backtracking_solution));
+        System.out.println("Gewicht: " + backtracking_knapsack.gewicht_beladung);
+        System.out.println("Wert: " + backtracking_knapsack.wert_beladung);
+        System.out.println("-----------------------------------------------------");
+        int[] greedy_solution = greedy_knapsack.greedyRucksack();
+        System.out.println("Greedy Rucksack: " + Arrays.toString(greedy_solution));
+        System.out.println("Gewicht: " + greedy_knapsack.gewicht_beladung);
+        System.out.println("Wert: " + greedy_knapsack.wert_beladung);
 
         // build random knapsack2
-        int[] gewichte2 = {10, 5, 7, 11};
-        int[] werte2 = {7, 6, 2, 20};
-        int maxgewicht2 = 30;
-        knapsack k2 = new knapsack(gewichte2, werte2, maxgewicht2);
-        int[] ausgewaehlt2 = k2.greedyRucksack();
-        System.out.println("Gewicht: " + k2.gewicht_beladung);
-        System.out.println("Wert: " + k2.wert_beladung);
-        System.out.print("Ausgewaehlt: ");
-        for (int j : ausgewaehlt2) {
-            System.out.print(j + " ");
-        }
-
-
-
+        int[] gewichte_2 = {10, 5, 7, 11};
+        int[] werte_2 = {7, 6, 2, 20};
+        int maxgewicht_2 = 30;
+        knapsack backtracking_knapsack_2 = new knapsack(gewichte_2, werte_2, maxgewicht_2);
+        knapsack greedy_knapsack_2 = new knapsack(gewichte_2, werte_2, maxgewicht_2);
+        int[] backtracking_solution_2 = backtracking_knapsack_2.backtracking();
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Backtracking Rucksack: " + Arrays.toString(backtracking_solution_2));
+        System.out.println("Gewicht: " + backtracking_knapsack_2.gewicht_beladung);
+        System.out.println("Wert: " + backtracking_knapsack_2.wert_beladung);
+        System.out.println("-----------------------------------------------------");
+        int[] greedy_solution_2 = greedy_knapsack_2.greedyRucksack();
+        System.out.println("Greedy Rucksack: " + Arrays.toString(greedy_solution_2));
+        System.out.println("Gewicht: " + greedy_knapsack_2.gewicht_beladung);
+        System.out.println("Wert: " + greedy_knapsack_2.wert_beladung);
+        System.out.println("-----------------------------------------------------");
     }
+
+
+
+
 }
